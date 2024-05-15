de67027da38d84d84495b2042dc142e349cd84fb
Jefferson De Andrade <jeffersonandrade1999@gmail.com>
2024-05-15 13:04:14 -0300

    STYLE: header update, with drop down menu made it

---
+++ src/Components/Parts/DropBoxHeader.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function DropBoxHeader(props){
+
+    const itemsStyle = "text-center px-2 hover:bg-slate-500 cursor-pointer rounded-lg mb-1";
+    return(
+        <div className="absolute top-12 right-3 my-transparent rounded-lg">
+            <ul>
+                <li className="flex justify-end"> 
+                    <div className="w-6 text-center mr-3 hover:bg-slate-400 cursor-pointer font-bold"
+                    onClick={() => props.settar(false)}>
+                    X</div>
+                </li>
+                <li className={itemsStyle}>Ver perfil</li>
+                <li className={itemsStyle}>Ver pedidos em andamento</li>
+                <li className={itemsStyle}>Ver estatisticas</li>
+            </ul>
+        </div>
+    )
+}
+
+export default DropBoxHeader;
--- src/Components/Parts/Header.jsx
+++ src/Components/Parts/Header.jsx
@@ -1,13 +1,21 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink} from "react-router-dom";
+import DropBoxHeader from "./DropBoxHeader";
 
 function Header (props){
 
-    const noActive = "text-2xl font-bold mx-3";
-    const active = "text-2xl font-bold mx-3 text-slate-700"
+    const noActive = "text-2xl mx-3 main-font text-yellow-100 font-light";
+    const active = "text-2xl mx-3 main-font font-semibold text-yellow-100"
+
+    const [dropAcitve, setDropActive] = useState(false);
+
+    const settar = (ativo) =>{
+        setDropActive(ativo);
+    }
+
     return(
 
-        <div className="w-full bg-zinc-950">
+        <div className="w-full bg-lime-700 fixed top-0">
             <nav className="flex justify-center h-12 w-full items-center">
                 <ul className="flex">
                     <NavLink to="/"
@@ -16,13 +24,23 @@ function Header (props){
 
                     <NavLink to="/pedidos"
                     className={props.page == "/pedidos" ? active : noActive}
-                    onClick={()=> props.mudarPage("/pedidos")}>FAZER PEDIDO</NavLink>
+                    onClick={()=> props.mudarPage("/pedidos")}>PEDIDO</NavLink>
 
                     <NavLink to="/checar"
                     className={props.page == "/checar" ? active : noActive}
-                    onClick={()=> props.mudarPage("/checar")}>CHECAR PEDIDOS</NavLink>
+                    onClick={()=> props.mudarPage("/checar")}>CONTA</NavLink>
+                    
                     
                 </ul>
+
+                <div className="border w-10 absolute top-1 right-7 h-10 rounded-full bg-white"
+                onClick={()=> setDropActive(true)}>
+                </div>
+                
+                {
+                    dropAcitve && <DropBoxHeader settar={settar}/>
+                }
+
             </nav>
         </div>
     )
