005868f665888aa4f1c4a6b75ce3a2367701b03a
Jefferson De Andrade <jeffersonandrade1999@gmail.com>
2024-05-23 17:02:40 -0300

    style: update style in conta page

--- src/Components/DropBoxMenu.jsx
+++ src/Components/DropBoxMenu.jsx
@@ -3,12 +3,13 @@ import { NavLink } from "react-router-dom";
 
 function DropBoxMenu(props){
 
-    const itemsStyle = "text-center px-2 hover:bg-slate-500 cursor-pointer rounded-lg mb-1 p-2";
+    const itemsStyle = "text-center px-2 hover:bg-slate-500 cursor-pointer mb-1 p-2 border-b border-zinc-700 h-full";
     return(
-    <div className="absolute top-12 left-0 my-transparent rounded-lg">
+    
+    <div className="absolute top-0 left-0 bg-zinc-800 h-screen w-1/2">
         <ul>
             <li className="flex justify-end"> 
-                <div className="w-6 text-center mr-3 hover:bg-slate-400 cursor-pointer font-bold"
+                <div className="w-6 text-center mr-3 hover:bg-slate-400 cursor-pointer font-bold text-2xl"
                 onClick={() => props.settar(false)}>
                 X</div>
             </li>
--- src/Components/FullPages/ChecarPedidos.jsx
+++ src/Components/FullPages/ChecarPedidos.jsx
@@ -6,6 +6,7 @@ function ChecarPedidos(){
     const [dummyData, setDummyData] = useState({
         nome:"Jefferson Andrade Chaves",
         ativo: true,
+        admin: false,
         endereco: "Av. Paulista",
         cellphone: "11707070",
         email: "jeff@jeff.com.br",
@@ -25,7 +26,23 @@ function ChecarPedidos(){
     return(
         <div className="w-full min-h-screen flex justify-center flex-wrap">
             <h1 className="mt-20 text-xl font-bold main-font text-lime-700 w-full text-center">DETALHES DA CONTA</h1>
-            <div className="h-96 w-full border"></div>
+            <div className="w-full flex justify-center items-center m-8">
+                <div className="w-1/4 text-center main-font mx-8">
+                    <h2 className="text-2xl mb-2">Detalhes gerais</h2>
+                    <p className="main-font">{dummyData.nome}</p>
+                    <p>
+                        {
+                            dummyData.admin ? <p className="text-red-600 font-bold">Administrador</p> : <p className="text-green-600 font-bold">Cliente</p>
+                        }
+                    </p>
+                </div>
+
+                <div className="w-1/4 text-center main-font mx-8">
+                    <h2 className="text-2xl mb-2">Detalhes para contato</h2>
+                    <p className="main-font">{dummyData.email}</p>
+                    <p className="main-font">{dummyData.cellphone}</p>
+                </div>
+            </div>
 
             <h2 className="text-xl font-bold main-font text-lime-700 w-full text-center">Pedidos ativos</h2>
             <div className="w-3/4 flex p-3 justify-center items-center flex-wrap">
